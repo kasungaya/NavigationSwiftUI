bb7f6479f32e32ef5cec36274f66dddb5213f2a9
Kasun Gaya <kasunice.s@gmail.com>
2022-06-16 09:31:51 +1000

    source code

---
+++ NavigationSwiftUI/NavigationSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationSwiftUIApp.swift
+//  NavigationSwiftUI
+//
+//  Created by kas gaya on 2022-06-16.
+//
+
+import SwiftUI
+
+@main
+struct NavigationSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
